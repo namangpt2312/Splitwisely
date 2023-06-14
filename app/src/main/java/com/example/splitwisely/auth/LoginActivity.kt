94ae8c0109c7d4cb11196df5951a4daf06c672e5
Naman Gupta <namangpt2312@gmail.com>
2023-06-15 04:03:18 +0530

    first commit

---
+++ app/src/main/java/com/example/splitwisely/auth/LoginActivity.kt
@@ -0,0 +1,64 @@
+package com.example.splitwisely.auth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
+import com.example.splitwisely.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.hbb20.CountryCodePicker
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var phoneNumber : String
+    private lateinit var countryCode : String
+    val phoneNumberEt: EditText by lazy {
+        findViewById<EditText>(R.id.phoneNumberEt)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val nextBtn = findViewById<Button>(R.id.nextBtn)
+        phoneNumberEt.addTextChangedListener{
+            nextBtn.isEnabled = !(it.isNullOrEmpty() || it.length < 10)
+        }
+
+        nextBtn.setOnClickListener {
+            checkNumber()
+        }
+    }
+
+    private fun checkNumber() {
+        countryCode = findViewById<CountryCodePicker>(R.id.ccp).selectedCountryCodeWithPlus
+        phoneNumber = countryCode + phoneNumberEt.text.toString()
+
+        notifyUser()
+    }
+
+    private fun notifyUser() {
+        MaterialAlertDialogBuilder(this).apply {
+            setMessage("We will be verifying the phone number : $phoneNumber\n" +
+                    "Is this OK, or would you like to edit the number?")
+            setPositiveButton("Ok") { _,_ ->
+                showOtpActivity()
+            }
+            setNegativeButton("Edit") { dialog, which ->
+                dialog.dismiss()
+            }
+            setCancelable(false)
+            create()
+            show()
+        }
+    }
+
+    private fun showOtpActivity() {
+        startActivity(
+            Intent(this, OtpActivity:: class.java).putExtra(
+            PHONE_NUMBER, phoneNumber))
+        finish()
+    }
+}
---
+++ app/src/main/java/com/example/splitwisely/models/User.kt
@@ -0,0 +1,22 @@
+package com.example.splitwisely.models
+
+data class User(
+    val name: String,
+    val imageUrl: String,
+    val thumbImage: String,
+    val uid: String,
+    val deviceToken: String,
+    val status: String,
+    val onlineStatus: String
+) {
+    constructor() : this("", "", "", "", "", "", "")
+    constructor(name: String, imageUrl: String, thumbImage: String, uid: String) : this(
+        name,
+        imageUrl,
+        thumbImage,
+        uid,
+        "",
+        "Hey there! I am using Messenger.",
+        ""
+    )
+}

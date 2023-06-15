b683f617c7c15765dc04597d1fbf206daa69fdd5
Naman Gupta <namangpt2312@gmail.com>
2023-06-15 07:47:33 +0530

    created groups

---
+++ app/src/main/java/com/example/splitwisely/models/Group.kt
@@ -0,0 +1,9 @@
+package com.example.splitwisely.models
+
+data class Group(
+    val name: String = "",
+    val imageUrl: String = "",
+    val uid: String = "",
+    val createdBy: String = "",
+    val createdAt: Long = 0L
+)
